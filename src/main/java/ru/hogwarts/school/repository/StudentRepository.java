4c4e8d82e7feef2a41f7d757d61c3eed90ab8649
Alex <alexsa-8@mail.ru>
2023-01-20 18:57:01 +0300

    SpringBoot-2

--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -1,7 +1,15 @@
 package ru.hogwarts.school.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school.model.Student;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository <Student, Long> {
+    List<Student> findByAgeEquals(int age);
+    Student findByName(String name);
+    Collection<Student> findStudentsByAge(int age);
+    Collection<Student> findAllByNameContains(String part);
 }
