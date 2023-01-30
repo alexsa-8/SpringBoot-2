c7e2a3ca0a66def7087ddefc6bf00b9715505907
Alex <alexsa-8@mail.ru>
2023-01-30 20:16:20 +0300

    SpringBoot-2

---
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -0,0 +1,30 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.service.AvatarService;
+
+import java.io.IOException;
+
+@RestController
+public class AvatarController {
+    private final AvatarService avatarService;
+
+    public AvatarController(AvatarService avatarService) {
+        this.avatarService = avatarService;
+    }
+
+    @PostMapping(value = "{id}/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadAvatar(@PathVariable Long id, @RequestParam MultipartFile avatar) throws IOException {
+        if (avatar.getSize() > 1024 * 300) {
+            return ResponseEntity.badRequest().body("Слишком большой файл.");
+        }
+        avatarService.uploadAvatar(id, avatar);
+        return ResponseEntity.ok().build();
+    }
+}
